98fe2f2188ac6ccfc031a46fd6cb85f1d9eb0ef9
B. Flores <56740397+Distorted-Dundar@users.noreply.github.com>
2022-06-28 23:46:05 -0400

    MorseCodeMain & MorseNode java

---
+++ src/MorseCode.java
@@ -0,0 +1,18 @@
+public class MorseCode {
+
+    public MorseCode(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println((int)'_');
+        System.out.println((int)'.');
+        System.out.println((int)'_');
+        System.out.println((int)'.');
+//        if(char.int ==95)
+//            root.Left
+//        if(char.int ==46)
+//            root.right
+    }
+
+}
---
+++ src/MorseNode.java
@@ -0,0 +1,10 @@
+public class MorseNode {
+    char data;
+    char meaning;
+    MorseNode Left,Right;
+
+
+
+
+
+}
